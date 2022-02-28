be25d6cde205add164ea570e17e59baa1fb33887
Sebastián Cruz Gómez <sebasbebe12@hotmail.com>
2022-02-28 14:47:06 -0600

    fix: corrige en estilos fin en pate 2

--- css/clase-9/reto-2/js/main.js
+++ css/clase-9/reto-2/js/main.js
@@ -1,10 +1,13 @@
 const caja = document.getElementById("caja");
+const caja1 = document.getElementById("caja1");
 const btn = document.getElementById("btn");
 
 btn.addEventListener('click', () => {
         caja.classList.add('mostrarCaja');
+        caja1.classList.add('mostrarCaja1');
         setInterval(() => {
-            caja.classList.add('ocultarCaja');
+            caja1.classList.remove('mostrarCaja1');
+            caja1.classList.add('ocultarCaja');
             
         }, 5000);
     });
