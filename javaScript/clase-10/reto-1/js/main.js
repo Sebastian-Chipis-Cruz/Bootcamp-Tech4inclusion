71876bfd3609231142b8aee9f965355872c0c4fc
Sebastián Cruz Gómez <sebasbebe12@hotmail.com>
2022-03-02 14:34:13 -0600

    fix: corrige en estilos

---
+++ javaScript/clase-10/reto-1/js/main.js
@@ -0,0 +1,7 @@
+const caja = document.getElementById("caja");
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+        caja.classList.add('caja-transition');
+    });
+
